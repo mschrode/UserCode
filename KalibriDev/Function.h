3471dc82c8cb884a57f6e48ea4c5ceb08df05058
mschrode <>
2010-02-09 10:16:30 +0000

    Interface to parameter errors and covariances

--- KalibriDev/Function.h
+++ KalibriDev/Function.h
@@ -2,13 +2,10 @@
 //    Class representing a correction function
 //
 //    first version: Hartmut Stadie 2008/12/14
-//    $Id: Function.h,v 1.3 2010/01/21 17:01:22 mschrode Exp $
+//    $Id: Function.h,v 1.4 2009/11/24 16:52:59 stadie Exp $
 //   
 #ifndef FUNCTION_H
 #define FUNCTION_H
-
-#include <cassert>
-#include <vector>
  
 class Measurement;
 class Parametrization;
@@ -17,18 +14,17 @@ class Parametrization;
 class Function {
   
   typedef double (Parametrization::*ParametrizationFunction)(const Measurement*, const double*) const; 
-  typedef double (Parametrization::*ParameterErrorFunction)(const Measurement*, const double*,const double*, const std::vector<int>&) const; 
 
  public:
   Function(ParametrizationFunction func, ParametrizationFunction invfunc,
 	   int parindex, int npars, double *firstpar, const Parametrization* p) 
-    : func_(func),invfunc_(invfunc),sigma_(0),firstpar_(firstpar),firstError_(0),
+    : func_(func),invfunc_(invfunc),firstpar_(firstpar),firstError_(0),
     cov_(0),parindex_(parindex),npars_(npars),param_(p)
     {}
   Function(ParametrizationFunction func, ParametrizationFunction invfunc,
-	   ParameterErrorFunction sigma, int parindex, int npars, double *firstpar,
-	   double *firstError, double *cov, const std::vector<int> &covIdx, const Parametrization* p) 
-    : func_(func),invfunc_(invfunc),sigma_(sigma),firstpar_(firstpar),firstError_(firstError),
+	   int parindex, int npars, double *firstpar, double *firstError, double *cov,
+	   const std::vector<int> &covIdx, const Parametrization* p) 
+    : func_(func),invfunc_(invfunc),firstpar_(firstpar),firstError_(firstError),
     cov_(cov),covIdx_(covIdx),parindex_(parindex),npars_(npars),param_(p)
     {}
   double* firstPar() const { return firstpar_;}
@@ -37,8 +33,8 @@ class Function {
   int parIndex() const { return parindex_;}
   int nPars() const { return npars_;}
   double operator()(const Measurement* x) const { return (param_->*func_)(x,firstpar_);}
-  double sigma(const Measurement* x) const { return (param_->*sigma_)(x,firstpar_,cov_,covIdx_); }
   void changeParBase(double* oldpar, double* newpar) { firstpar_ += newpar - oldpar;}
+
   double par(int i) const { assert( i >= 0 && i < nPars() ); return firstPar()[i]; }
   double parError(int i) const { assert( i >= 0 && i < nPars() ); return firstError()[i]; }
   double parCov(int i, int j) const;
@@ -49,7 +45,6 @@ class Function {
  private:
   const ParametrizationFunction func_;
   const ParametrizationFunction invfunc_;
-  const ParameterErrorFunction sigma_;
   double *firstpar_;
   double *firstError_;
   double *cov_;
