4bfad1ee198737af1bf92cc2703bc608ccb1ba27
mschrode <>
2010-03-09 20:21:45 +0000

    Methods to set some predefined gStyles

---
+++ util/StyleSettings.h
@@ -0,0 +1,202 @@
+#ifndef STYLE_SETTINGS_H
+#define STYLE_SETTINGS_H
+
+#include <iostream>
+
+#include "TString.h"
+#include "TStyle.h"
+
+
+
+namespace util {
+  // -------------------------------------------
+  class StyleSettings {
+  public:
+    static void screen() { setStyle("Screen",true); }
+    static void screenNoTitle() { setStyle("Screen",false); }
+    static void presentation() { setStyle("Presentation",true); }
+    static void presentationNoTitle() { setStyle("Presentation",false); }
+    static int color(int i);
+    
+  private:
+    static void setStyle(const TString &mode, bool spaceForTitle);
+  };
+
+
+  void StyleSettings::setStyle(const TString &mode, bool spaceForTitle) {
+    //  For 'colz' TH2
+    gStyle->SetPalette(1);
+    
+    //  For the canvas
+    gStyle->SetCanvasBorderMode(0);
+    gStyle->SetCanvasColor(kWhite);
+    gStyle->SetCanvasDefH(800); //Height of canvas
+    gStyle->SetCanvasDefW(800); //Width of canvas
+    gStyle->SetCanvasDefX(0);   //Position on screen
+    gStyle->SetCanvasDefY(0);
+    
+    //  For the frame
+    gStyle->SetFrameBorderMode(0);
+    gStyle->SetFrameBorderSize(1);
+    gStyle->SetFrameFillColor(kBlack);
+    gStyle->SetFrameFillStyle(0);
+    gStyle->SetFrameLineColor(kBlack);
+    gStyle->SetFrameLineStyle(0);
+    gStyle->SetFrameLineWidth(1);
+    
+    //  For the Pad
+    gStyle->SetPadBorderMode(0);
+    gStyle->SetPadColor(kWhite);
+    gStyle->SetPadGridX(false);
+    gStyle->SetPadGridY(false);
+    gStyle->SetGridColor(0);
+    gStyle->SetGridStyle(3);
+    gStyle->SetGridWidth(1);
+
+    //  Margins
+    if( mode == "Presentation" ) {
+      if( spaceForTitle ) {
+	gStyle->SetPadTopMargin(0.11);
+	gStyle->SetPadBottomMargin(0.18);
+	gStyle->SetPadLeftMargin(0.25);
+	gStyle->SetPadRightMargin(0.04);
+      } else {
+	gStyle->SetPadTopMargin(0.05);
+	gStyle->SetPadBottomMargin(0.18);
+	gStyle->SetPadLeftMargin(0.19);
+	gStyle->SetPadRightMargin(0.04);
+      }
+    } else {
+      if( spaceForTitle ) {
+	gStyle->SetPadTopMargin(0.09);
+	gStyle->SetPadBottomMargin(0.14);
+	gStyle->SetPadLeftMargin(0.19);
+	gStyle->SetPadRightMargin(0.04);
+      } else {
+	gStyle->SetPadTopMargin(0.05);
+	gStyle->SetPadBottomMargin(0.14);
+	gStyle->SetPadLeftMargin(0.17);
+	gStyle->SetPadRightMargin(0.04);
+      }
+    }
+
+    //  For the histo:
+    gStyle->SetHistLineColor(kBlack);
+    gStyle->SetHistLineStyle(0);
+    gStyle->SetHistLineWidth(1);
+    
+    //  For the statistics box:
+    if( mode == "Presentation" ) {
+      gStyle->SetOptStat(0);
+    } else {
+      gStyle->SetOptStat("eMR");
+      gStyle->SetStatColor(kWhite);
+      gStyle->SetStatFont(42);
+      gStyle->SetStatFontSize(0.03);
+      gStyle->SetStatTextColor(1);
+      gStyle->SetStatFormat("6.4g");
+      gStyle->SetStatBorderSize(1);
+      gStyle->SetStatX(0.94);              
+      gStyle->SetStatY(0.86);              
+      gStyle->SetStatH(0.16);
+      gStyle->SetStatW(0.22);
+    }
+    
+    //  For the Global title:
+    gStyle->SetOptTitle(1);
+    gStyle->SetTitleFont(42,"");
+    gStyle->SetTitleColor(1);
+    gStyle->SetTitleTextColor(1);
+    gStyle->SetTitleFillColor(0);
+    gStyle->SetTitleFontSize(0.1);
+    gStyle->SetTitleAlign(23);
+    gStyle->SetTitleX(0.6);
+    gStyle->SetTitleH(0.05);
+    gStyle->SetTitleBorderSize(0);
+
+    //  For the axis
+    gStyle->SetAxisColor(1,"XYZ");
+    gStyle->SetStripDecimals(kTRUE);
+    gStyle->SetTickLength(0.03,"XYZ");
+    gStyle->SetNdivisions(510,"XYZ");
+    gStyle->SetPadTickX(1);  // To get tick marks on the opposite side of the frame
+    gStyle->SetPadTickY(1);
+    
+    //  For the axis labels and titles
+    gStyle->SetTitleColor(1,"XYZ");
+    gStyle->SetLabelColor(1,"XYZ");
+    if( mode == "Presentation" ) {
+      // For the axis labels:
+      gStyle->SetLabelFont(42,"XYZ");
+      gStyle->SetLabelOffset(0.007,"XYZ");
+      gStyle->SetLabelSize(0.045,"XYZ");
+      
+      // For the axis titles:
+      gStyle->SetTitleFont(42,"XYZ");
+      gStyle->SetTitleSize(0.06,"XYZ");
+      gStyle->SetTitleXOffset(1.2);
+      if( spaceForTitle ) gStyle->SetTitleYOffset(2.0);
+      else                 gStyle->SetTitleYOffset(1.5);
+    } else {
+      // For the axis labels:
+      gStyle->SetLabelFont(42,"XYZ");
+      gStyle->SetLabelOffset(0.007,"XYZ");
+      gStyle->SetLabelSize(0.04,"XYZ");
+      
+      // For the axis titles:
+      gStyle->SetTitleFont(42,"XYZ");
+      gStyle->SetTitleSize(0.04,"XYZ");
+      gStyle->SetTitleXOffset(1.5);
+      if( spaceForTitle ) gStyle->SetTitleYOffset(2.1);
+      else                 gStyle->SetTitleYOffset(1.6);
+    }
+
+    //  For the legend
+    gStyle->SetLegendBorderSize(0);
+
+    //  For the statistics box
+    if( mode == "Presentation" ) {
+      if( spaceForTitle ) {
+	gStyle->SetStatFontSize(0.04);
+	gStyle->SetStatX(0.92);              
+	gStyle->SetStatY(0.86);              
+	gStyle->SetStatH(0.2);
+	gStyle->SetStatW(0.3);
+      } else {
+	gStyle->SetStatFontSize(0.04);
+	gStyle->SetStatX(0.92);              
+	gStyle->SetStatY(0.92);              
+	gStyle->SetStatH(0.2);
+	gStyle->SetStatW(0.3);
+      }
+    } else {
+      if( spaceForTitle ) {
+	gStyle->SetStatFontSize(0.03);
+	gStyle->SetStatX(0.92);              
+	gStyle->SetStatY(0.86);              
+	gStyle->SetStatH(0.16);
+	gStyle->SetStatW(0.22);
+      } else {
+	gStyle->SetStatFontSize(0.03);
+	gStyle->SetStatX(0.92);              
+	gStyle->SetStatY(0.92);              
+	gStyle->SetStatH(0.16);
+	gStyle->SetStatW(0.22);
+      }
+    }
+
+    std::cout << "Adjusted gStyle for " << std::flush;
+    if( mode == "Screen" ) std::cout << "screen viewing" << std::flush;
+    else std::cout << "presentations" << std::flush;
+    std::cout << " and " << std::flush;
+    if( spaceForTitle ) std::cout << "histograms with title." << std::endl;
+    else std::cout << "histograms without title." << std::endl;
+  }
+
+  
+  int StyleSettings::color(int i) {
+    int col[5] = { 1, 2, 4, 7, 8 };
+    return (i>=0 && i<5) ? col[i] : 1;
+  }
+}
+#endif
