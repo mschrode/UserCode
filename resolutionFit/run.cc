434d43b07d9bbeef53ef95d0b744dac72fbfa25a
mschrode <>
2010-05-04 19:20:08 +0000

    Paths to latest fit result

--- resolutionFit/run.cc
+++ resolutionFit/run.cc
@@ -1,4 +1,4 @@
-// $Id: run.cc,v 1.4 2010/03/23 20:00:39 mschrode Exp $
+// $Id: run.cc,v 1.5 2010/04/01 11:00:02 mschrode Exp $
 
 #include <cassert>
 #include <vector>
@@ -10,43 +10,42 @@
 
 int main() {
 
-  TString baseNameStdSel = "~/results/ResolutionFit/Gauss/PtBins/StandardSelection/gauss_StdSel_";
+  TString baseNameStdSel = "~/results/ResolutionFit/Gauss/resolutionSpring10_Gauss_";
 
   std::vector<TString> baseCutVars;
-  baseCutVars.push_back("~/results/ResolutionFit/Gauss/PtBins/Rel3rdPt006/gauss_Rel3rdPt006_");
-  baseCutVars.push_back("~/results/ResolutionFit/Gauss/PtBins/Rel3rdPt008/gauss_Rel3rdPt008_");
-  baseCutVars.push_back("~/results/ResolutionFit/Gauss/PtBins/StandardSelection/gauss_StdSel_");
-  baseCutVars.push_back("~/results/ResolutionFit/Gauss/PtBins/Rel3rdPt012/gauss_Rel3rdPt012_");
-  baseCutVars.push_back("~/results/ResolutionFit/Gauss/PtBins/Rel3rdPt015/gauss_Rel3rdPt015_");
-  baseCutVars.push_back("~/results/ResolutionFit/Gauss/PtBins/Rel3rdPt020/gauss_Rel3rdPt020_");
+  baseCutVars.push_back("~/results/ResolutionFit/Gauss/resolutionSpring10_Gauss_Rel3rdPt006_");
+  baseCutVars.push_back("~/results/ResolutionFit/Gauss/resolutionSpring10_Gauss_Rel3rdPt008_");
+  baseCutVars.push_back("~/results/ResolutionFit/Gauss/resolutionSpring10_Gauss_");
+  baseCutVars.push_back("~/results/ResolutionFit/Gauss/resolutionSpring10_Gauss_Rel3rdPt012_");
+  baseCutVars.push_back("~/results/ResolutionFit/Gauss/resolutionSpring10_Gauss_Rel3rdPt020_");
 
   std::vector<double> ptCuts;
   ptCuts.push_back(0.06);
   ptCuts.push_back(0.08);
   ptCuts.push_back(0.1);
   ptCuts.push_back(0.12);
-  ptCuts.push_back(0.15);
+//   ptCuts.push_back(0.15);
   ptCuts.push_back(0.20);
 
-  TString baseMCStat = "~/results/ResolutionFit/Gauss/PtBins/Flat/gauss_Flat_StdSel_";
+  TString baseMCStat = "~/results/ResolutionFit/Gauss/resolutionSpring10_Gauss_Flat_";
 
   std::vector<TString> baseSystUp;
 //   baseSystUp.push_back("~/results/ResolutionFit/Gauss/PtBins/SigmaUp30/gauss_SigmaUp30_StdSel_");
 //   baseSystUp.push_back("~/results/ResolutionFit/Gauss/PtBins/SigmaDown30/gauss_SigmaDown30_StdSel_");
-  baseSystUp.push_back("~/results/ResolutionFit/Gauss/PtBins/SlopeUp50/gauss_SlopeUp50_StdSel_");
-  baseSystUp.push_back("~/results/ResolutionFit/Gauss/PtBins/SlopeDown50/gauss_SlopeDown50_StdSel_");
+//   baseSystUp.push_back("~/results/ResolutionFit/Gauss/PtBins/SlopeUp50/gauss_SlopeUp50_StdSel_");
+//   baseSystUp.push_back("~/results/ResolutionFit/Gauss/PtBins/SlopeDown50/gauss_SlopeDown50_StdSel_");
 
   std::vector<TString> baseSystDown;
 //   baseSystDown.push_back("~/results/ResolutionFit/Gauss/PtBins/SigmaUp30/gauss_SigmaUp30_StdSel_");
 //   baseSystDown.push_back("~/results/ResolutionFit/Gauss/PtBins/SigmaDown30/gauss_SigmaDown30_StdSel_");
-  baseSystDown.push_back("~/results/ResolutionFit/Gauss/PtBins/SlopeUp50/gauss_SlopeUp50_StdSel_");
-  baseSystDown.push_back("~/results/ResolutionFit/Gauss/PtBins/SlopeDown50/gauss_SlopeDown50_StdSel_");
+//   baseSystDown.push_back("~/results/ResolutionFit/Gauss/PtBins/SlopeUp50/gauss_SlopeUp50_StdSel_");
+//   baseSystDown.push_back("~/results/ResolutionFit/Gauss/PtBins/SlopeDown50/gauss_SlopeDown50_StdSel_");
 
   std::vector<TString> labelSyst;
 //   labelSyst.push_back("#sigma_{MC} +30%");
 //   labelSyst.push_back("#sigma_{MC} -30%");
-  labelSyst.push_back("Spectrum +50%");
-  labelSyst.push_back("Spectrum -50%");
+//   labelSyst.push_back("Spectrum +50%");
+//   labelSyst.push_back("Spectrum -50%");
 
   std::vector<double> ptBinEdges;
   //  ptBinEdges.push_back(80.);
@@ -62,8 +61,8 @@ int main() {
   ptBinEdges.push_back(1000.);
 
   std::vector<resolutionFit::PtBin*> ptBins;
-  int start = 2; 
-  int end  = 10;
+  int start = 0; 
+  int end  = 8;
 
   assert( ptBinEdges.size() >= (2+end-start) );
   assert( baseSystUp.size() == baseSystDown.size() );
@@ -100,8 +99,8 @@ int main() {
   }  
   std::vector<double> trueResPar;
   trueResPar.push_back(0.);
-  trueResPar.push_back(1.145);
-  trueResPar.push_back(0.037);
+  trueResPar.push_back(1.15);
+  trueResPar.push_back(0.04);
   resolutionFit::FittedResolution *fit = new resolutionFit::FittedResolution(ptBins,trueResPar,"resFit_QCD_Gauss_");
   // Plot extrapolation per bin
   fit->plotExtrapolation();
