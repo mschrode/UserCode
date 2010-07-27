95ccdde995342e8cea1890b093345fb69e83e83f
mschrode <>
2010-07-27 10:05:43 +0000

    Encapsulates useful methods

---
+++ util/utils.h
@@ -0,0 +1,57 @@
+// $Id: $
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+#include <sstream>
+
+#include "TString.h"
+
+
+//!  Encapsulates useful classes and methods
+//!
+//!  \author   Matthias Schroeder (www.desy.de/~matsch)
+//!  \date     2010/03/09
+//!  $Id: $
+// -------------------------------------------------------------------------------------
+namespace util {
+
+  // -------------------------------------------------------------------------------------
+  static double round(double d, int decPlaces) {
+    d *= pow(10.,1.*decPlaces);
+    d = std::floor(d+0.5);
+    d /= pow(10.,1.*decPlaces);
+    
+    return d;
+  }  
+
+
+  // -------------------------------------------------------------------------------------
+  static std::string toString(double d) {
+    std::stringstream ss;
+    ss << d;
+    return ss.str();
+  }
+  
+  
+  // -------------------------------------------------------------------------------------
+  static std::string toString(double d, int decPlaces) {
+    std::stringstream ss;
+    ss << round(d,decPlaces);
+    return ss.str();
+  }
+
+
+  // -------------------------------------------------------------------------------------
+  static TString toTString(double d) {
+    return toString(d).c_str();
+  }
+  
+  
+  // -------------------------------------------------------------------------------------
+  static TString toTString(double d, int decPlaces) {
+    return toString(d,decPlaces).c_str();
+  }
+}
+#endif
