f141291e6f660db040421d438c161cd50df23f91
mschrode <>
2010-12-02 22:38:00 +0000

    running version

--- scripts/createScaledResponse.C
+++ scripts/createScaledResponse.C
@@ -3,8 +3,10 @@
 #include <vector>
 
 #include "TCanvas.h"
+#include "TF1.h"
 #include "TH1.h"
 #include "TH1D.h"
+#include "TPaveText.h"
 
 #include "../util/utils.h"
 #include "../util/FileOps.h"
@@ -17,6 +19,9 @@
 
 
 const double nSigCore_ = 2.;
+const double nSigTail_ = 3.;
+const double min_ = 3E-5;
+const double max_ = 3E3;
 
 TH1* hScalingFactors_ = 0;
 TH1* hScalingFactorsUp_ = 0;
@@ -24,14 +29,13 @@ TH1* hScalingFactorsDown_ = 0;
 
 
 
-std::vector<double> getSmearFactors(double pt) {
-  std::vector<double> v(1,1.1);
-  return v;
+double getSmearFactors(double pt) {
+  return 0.;
 }
 
 
 
-double getTailScalingFactor(const TH1* hFactors, double pt) {
+double getTailScalingFactor(TH1* hFactors, double pt) {
   double min = hFactors->GetXaxis()->GetBinLowEdge(1);
   double max = hFactors->GetXaxis()->GetBinUpEdge(hFactors->GetNbinsX());
   double fac = hFactors->GetBinContent(1);
@@ -53,19 +57,43 @@ double getTailScalingFactor(double pt) {
 
 
 void createScaledResponse() {
+  util::StyleSettings::presentationNoTitle();
 
   // Get smearing and tail scaling factors
   std::cout << "Getting smearing and tail scaling factors\n";
-  hScalingFactors_ = readTH1("","","hScalingFactors_");
-  hScalingFactorsUp_ = readTH1("","","hScalingFactorsUp_");
-  hScalingFactorsDown_ = readTH1("","","hScalingFactorsDown_");
+  TString fileScalingFactors = "Tails_PF_.root";
+  hScalingFactors_ = util::FileOps::readTH1(fileScalingFactors,"hScaleFactorsInt_Eta0","hScalingFactors_");
+  hScalingFactorsUp_ = util::FileOps::readTH1(fileScalingFactors,"hScaleFactorsIntUp_Eta0","hScalingFactorsUp_");
+  hScalingFactorsDown_ = util::FileOps::readTH1(fileScalingFactors,"hScaleFactorsIntDown_Eta0","hScalingFactorsDown_");
 
 
   // Get MC truth response histograms
   std::cout << "Getting MC truth response histograms\n";
   std::vector<double> ptGenBinEdges;
+  ptGenBinEdges.push_back(10.);
+  ptGenBinEdges.push_back(20.);
+  ptGenBinEdges.push_back(30.);
+  ptGenBinEdges.push_back(40.);
+  ptGenBinEdges.push_back(50.);
+  ptGenBinEdges.push_back(60.);
+  ptGenBinEdges.push_back(70.);
+  ptGenBinEdges.push_back(80.);
+  ptGenBinEdges.push_back(90.);
+  ptGenBinEdges.push_back(100.);
+  ptGenBinEdges.push_back(120.);
+  ptGenBinEdges.push_back(150.);
+  ptGenBinEdges.push_back(170.);
+  ptGenBinEdges.push_back(200.);
+  ptGenBinEdges.push_back(250.);
+  ptGenBinEdges.push_back(300.);
+  ptGenBinEdges.push_back(350.);
+  ptGenBinEdges.push_back(400.);
+  ptGenBinEdges.push_back(500.);
+  ptGenBinEdges.push_back(1000.);
+  ptGenBinEdges.push_back(1500.);
+
   //  10 20 30 40 50 60 70 80 90 100 120 150 170 200 250 300 350 400 500 1000 1500
-  util::HistVec hResMC = util::FileOps::readHistVec("","hRespMeas_");
+  util::HistVec hResMC = util::FileOps::readHistVec("MCFall10_TruthResolution_PF.root","hRespMeas_");
 
 
   // Sanity checks
@@ -84,14 +112,19 @@ void createScaledResponse() {
   util::HistVec hResSmeared(nPtBins);
   for(unsigned int i = 0; i < nPtBins; ++i) {
     hResMC[i]->GetXaxis()->SetRangeUser(0.,2.);
+    hResMC[i]->GetYaxis()->SetRangeUser(min_,max_);
+    util::HistOps::setAxisTitles(hResMC[i],"Response","","jets",true);
+    //    hResMC[i]->SetTitle("");    
     double width = 0.;
     double widthErr = 1000.;
     if( func::fitCoreWidth(hResMC[i],nSigCore_,width,widthErr) ) {
+      double ratioDataMC = getSmearFactors(ptGenBinCenters[i]);
+      std::cout << "  PtBin " << i << ": Data / MC (Core Width) = " << 1+ratioDataMC << std::endl;
       func::smearHistogram(hResMC[i],
 			   hResSmeared[i],
 			   hResMC[i]->GetEntries(),
 			   width,
-			   getSmearFactors(ptBinCenters[i]));
+			   ratioDataMC);
     } else {
       std::cerr << "ERROR: Fit of Gaussian core failed." << std::endl;
       exit(1);
@@ -106,9 +139,9 @@ void createScaledResponse() {
   util::HistVec hTailsClean(nPtBins);
   std::vector<TF1*> fGauss(nPtBins);
   for(unsigned int i = 0; i < nPtBins; ++i) {
-    func::getTail(hResSmeared[i],nSigTail_,hTails[i],hTailsClean[i],fGauss[i]);
+    func::getTail(hResSmeared[i],nSigCore_,nSigTail_,hTails[i],hTailsClean[i],fGauss[i]);
     hCore[i] = static_cast<TH1D*>(hResSmeared[i]->Clone("hCore"+util::toTString(i)));
-    hCore[i]->Add(hTailsClean[i],-1.);
+    hCore[i]->Add(hTails[i],-1.);
     hCore[i]->SetLineWidth(2);
   }
 
@@ -118,9 +151,11 @@ void createScaledResponse() {
   util::HistVec hResScaled(nPtBins);
   util::HistVec hTailsScaled(nPtBins);
   for(unsigned int i = 0; i < nPtBins; ++i) {
-    hTailsScaled[i] = static_cast<TH1D*>(hTailsClean[i]->Clone("hTailsScaled"+util::toTString(i)));
-    hTailsScaled[i]->Scale(getTailScalingFactor(ptGenBinCenters[i]));
-    hTailsClean[i]->SetLineStyle(2);
+    double tailScale = getTailScalingFactor(ptGenBinCenters[i]);
+    std::cout << "  PtBin " << i << ": Tail Scaling Factor " << tailScale << std::endl;
+    hTailsScaled[i] = static_cast<TH1D*>(hTails[i]->Clone("hTailsScaled"+util::toTString(i)));
+    hTailsScaled[i]->Scale(tailScale);
+    hTails[i]->SetLineStyle(2);
     hTailsScaled[i]->SetLineColor(2);
     hResScaled[i] = static_cast<TH1D*>(hResSmeared[i]->Clone("hResScaled"+util::toTString(i)));
     hResScaled[i]->Reset();
@@ -132,7 +167,7 @@ void createScaledResponse() {
 
 
    // Plots
-   for(unsigned int i = 0; i < nPtBins; ++i) {
+   for(unsigned int i = 10; i < nPtBins; ++i) {
      TCanvas* canSmear = new TCanvas("canSmear"+util::toTString(i),"Smeared "+util::toTString(i),500,500);
      canSmear->cd();
      hResSmeared[i]->GetXaxis()->UnZoom();
@@ -144,13 +179,15 @@ void createScaledResponse() {
 
      TCanvas* canParts = new TCanvas("canParts"+util::toTString(i),"Parts "+util::toTString(i),500,500);
      canParts->cd();
+     hCore[i]->GetYaxis()->SetRangeUser(min_,max_);
      hCore[i]->Draw("HIST");
      hTailsScaled[i]->Draw("HISTsame");
-     hTailsClean[i]->Draw("HISTsame");
+     hTails[i]->Draw("HISTsame");
      canParts->SetLogy();
 
      TCanvas* canScaled = new TCanvas("canScaled"+util::toTString(i),"Scaled Resp "+util::toTString(i),500,500);
      canScaled->cd();
+     hResSmeared[i]->GetYaxis()->SetRangeUser(min_,max_);
      hResSmeared[i]->Draw("HIST");
      hResScaled[i]->Draw("HISTsame");
      canScaled->SetLogy();
