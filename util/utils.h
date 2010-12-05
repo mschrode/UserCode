fd6a8d14e257d7a1c5c1a3b8ea6cb3ac0a825d79
mschrode <>
2010-12-05 22:19:26 +0000

    Keyword inline to remove compiler warnings

--- util/utils.h
+++ util/utils.h
@@ -1,4 +1,4 @@
-// $Id: utils.h,v 1.4 2010/08/21 13:40:21 mschrode Exp $
+// $Id: utils.h,v 1.5 2010/11/23 11:25:09 mschrode Exp $
 
 #ifndef UTILS_H
 #define UTILS_H
@@ -15,20 +15,20 @@
 //!
 //!  \author   Matthias Schroeder (www.desy.de/~matsch)
 //!  \date     2010/03/09
-//!  $Id: utils.h,v 1.4 2010/08/21 13:40:21 mschrode Exp $
+//!  $Id: utils.h,v 1.5 2010/11/23 11:25:09 mschrode Exp $
 // -------------------------------------------------------------------------------------
 namespace util {
 
   //! Returns error of A / B for two uncorrelated variables
   //! A, B with error AE, BE
   // -------------------------------------------------------------------------------------
-  static double ratioError(double A, double AE, double B, double BE) {
+  static inline double ratioError(double A, double AE, double B, double BE) {
     return sqrt( AE*AE/B/B + BE*BE*A*A/B/B/B/B );
   }
 
 
   // -------------------------------------------------------------------------------------
-  static double round(double d, int decPlaces) {
+  static inline double round(double d, int decPlaces) {
     d *= pow(10.,1.*decPlaces);
     d = std::floor(d+0.5);
     d /= pow(10.,1.*decPlaces);
@@ -38,7 +38,7 @@ namespace util {
 
 
   // -------------------------------------------------------------------------------------
-  static std::string toString(double d) {
+  static inline std::string toString(double d) {
     std::stringstream ss;
     ss << d;
     return ss.str();
@@ -46,7 +46,7 @@ namespace util {
   
   
   // -------------------------------------------------------------------------------------
-  static std::string toString(double d, int decPlaces) {
+  static inline std::string toString(double d, int decPlaces) {
     std::stringstream ss;
     ss << round(d,decPlaces);
     return ss.str();
@@ -54,19 +54,19 @@ namespace util {
 
 
   // -------------------------------------------------------------------------------------
-  static TString toTString(double d) {
+  static inline TString toTString(double d) {
     return toString(d).c_str();
   }
   
   
   // -------------------------------------------------------------------------------------
-  static TString toTString(double d, int decPlaces) {
+  static inline TString toTString(double d, int decPlaces) {
     return toString(d,decPlaces).c_str();
   }
 
 
   // -------------------------------------------------------------------------------------
-  static bool findBin(double x, const std::vector<double> &binEdges, unsigned int &bin) {
+  static inline bool findBin(double x, const std::vector<double> &binEdges, unsigned int &bin) {
     bin = 0;
     bool inRange = false;
     if( x >= binEdges.front() && x <= binEdges.back() ) {
