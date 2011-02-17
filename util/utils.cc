aa3618a431bbda124503acb9af03b19965a9dda5
mschrode <>
2011-02-17 13:26:13 +0000

    For inclusion into external projects

---
+++ util/utils.cc
@@ -0,0 +1,17 @@
+// $Id: $
+
+#include "ConfigParser.h"
+#include "FileOps.h"
+#include "HistOps.h"
+#include "LabelFactory.h"
+#include "StyleSettings.h"
+#include "utils.h"
+
+
+std::vector<TH1*> util::LabelFactory::hDummies_;
+unsigned int util::HistOps::COUNT_ = 0;
+
+
+
+
+
