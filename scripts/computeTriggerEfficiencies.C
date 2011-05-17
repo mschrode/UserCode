6b8d4c581952991620479d3771984896bf64922b
mschrode <>
2011-05-17 16:39:11 +0000

    new method

--- scripts/computeTriggerEfficiencies.C
+++ scripts/computeTriggerEfficiencies.C
@@ -1,4 +1,4 @@
-// $Id: computeTriggerEfficiencies.C,v 1.2 2011/04/16 10:07:33 mschrode Exp $
+// $Id: computeTriggerEfficiencies.C,v 1.3 2011/05/09 18:37:25 mschrode Exp $
 
 #include <algorithm>
 #include <cmath>
@@ -23,6 +23,27 @@
 
 
 // Trigger efficiency plots from Kalibri ntuple
+//
+// Note: traditionally you would compute the efficiency of a
+// trigger A using a reference trigger B as
+//
+//   eff = N(passing A && passing B) / N(passing B) .
+//
+// However, due to the sometimes large pre-scales there might
+// only be very few events available for this method preventing
+// a reliable measurement. Instead, the efficiency is defined
+// as
+//
+//   eff = N(passing A) / N(passing B) .
+//
+// Of course, now 0 <= eff <= 1 is not true anymore but without
+// knowledge of the pre-scales the previous definition is not
+// exact either. The pt of the turn-on can still be correctly
+// determined as the pt where eff is 99% of the plateau value
+// of eff.
+//
+// TODO: Re-write to run over all triggers at the same time and 
+// divide the different histograms.
 
 
 
@@ -96,7 +117,9 @@ void computeTriggerEfficiences(const TString &fileNames, int runStart, int runEn
   std::cout << "Creating histograms...\n";
   // Event counts
   TH1 *hTotal = util::HistOps::createTH1D("hTotal",nBins,minPtAve,maxPtAve,"p^{ave}_{T}","GeV","events");
+  hTotal->Sumw2();
   TH1 *hPass = util::HistOps::createTH1D("hPass",nBins,minPtAve,maxPtAve,"p^{ave}_{T}","GeV","events");
+  hPass->Sumw2();
 
 
   std::cout << "Adding files to chain...\n";
@@ -139,7 +162,7 @@ void computeTriggerEfficiences(const TString &fileNames, int runStart, int runEn
       continue;
     }
 
-    if( nObjJet > 1 && hltTag ) {
+    if( nObjJet > 1 && (hltTag || hltProbe) ) {
       ++nTotal;
 
       // Sort jets by corrected pt
@@ -153,7 +176,10 @@ void computeTriggerEfficiences(const TString &fileNames, int runStart, int runEn
       double ptAve = 0.5*(corrJetPt[corrJetPt.size()-1]+corrJetPt[corrJetPt.size()-2]);
       
       // Fill histogram of all events
-      hTotal->Fill(ptAve);
+      if( hltTag ) {
+	++nTotal;
+	hTotal->Fill(ptAve);
+      }
 
       // If triggered by probe trigger, fill histogram of
       // events passing
@@ -164,17 +190,20 @@ void computeTriggerEfficiences(const TString &fileNames, int runStart, int runEn
     } // End if( hltTag )
   } // End loop over entries
 
-  std::cout << "\n\n--------------------------------------\n";
+  std::cout << "\n\n------------------------------------------\n";
   std::cout << "  Entries                    \t" << nEvts << std::endl;
-  std::cout << "  Total (" << tagTrigger << ") \t" << nTotal << std::endl;
-  std::cout << "  Passed (" << probeTrigger << ") \t" << nPass << std::endl;
-  std::cout << "--------------------------------------\n\n";
-
+  std::cout << "  N(passing " << tagTrigger << ") \t" << nTotal << std::endl;
+  std::cout << "  N(passing " << probeTrigger << ") \t" << nPass << std::endl;
+  std::cout << "------------------------------------------\n\n";
 
   // Efficiency graph
-  TGraphAsymmErrors *gEff = new TGraphAsymmErrors();
-  gEff->BayesDivide(hPass,hTotal);
-  gEff->SetMarkerStyle(20);
+//   TGraphAsymmErrors *gEff = new TGraphAsymmErrors();
+//   gEff->BayesDivide(hPass,hTotal);
+//   gEff->SetMarkerStyle(20);
+
+  TH1* hEff = static_cast<TH1*>(hPass->Clone("hEff"));
+  hEff->Divide(hPass,hTotal,1,1);
+  hEff->SetMarkerStyle(20);
 
   // Fit efficiency
   TF1* fit = new TF1("efficiency",eff,minPtAve,maxPtAve,3);
@@ -182,7 +211,8 @@ void computeTriggerEfficiences(const TString &fileNames, int runStart, int runEn
   fit->SetParameter(1,0.5*(minPtAve+maxPtAve));
   fit->SetParameter(2,1.);
   fit->SetLineColor(kBlue);
-  gEff->Fit(fit,"0QR");
+  //gEff->Fit(fit,"0QR");
+  hEff->Fit(fit,"0QR");
 
   // Determine 99% threshold
   double ptAve99 = fit->GetX(0.99*fit->GetParameter(2),minPtAve,maxPtAve);
@@ -195,23 +225,24 @@ void computeTriggerEfficiences(const TString &fileNames, int runStart, int runEn
   // Label efficiency
   TPaveText *label = util::LabelFactory::createPaveText(3);
   label->AddText(jetLabel+", Runs "+util::toTString(runStart)+" - "+util::toTString(runEnd));
-  if( lumi > 0. ) label->AddText("Tag trigger "+tagTrigger+" ("+util::toTString(lumi)+"/pb)");
+  if( lumi > 0. ) label->AddText("L = "+util::toTString(lumi)+"/pb,  Reference: "+tagTrigger);
   else label->AddText("Tag trigger "+tagTrigger);
   label->AddText("p^{ave}_{T}(#epsilon > 99%) = "+util::toTString(ptAve99)+" GeV");
   label->GetLine(2)->SetTextColor(lPtAve99->GetLineColor());
 
-  TH1 *hFrame = util::HistOps::createRatioFrame(minPtAve,maxPtAve,0.,1.59*fit->GetParameter(2),"p^{ave}_{T} (GeV)",probeTrigger+" Efficiency");
+  TH1 *hFrame = util::HistOps::createRatioFrame(minPtAve,maxPtAve,0.,1.8*fit->GetParameter(2),"p^{ave}_{T} (GeV)",probeTrigger+" Efficiency");
   hFrame->SetLineColor(fit->GetLineColor());
   for(int i = 1; i <= hFrame->GetNbinsX(); ++i) {
     hFrame->SetBinContent(i,fit->GetParameter(2));
   }
 
-  TString outNamePrefix = "Efficiency_"+jetType+"_"+probeTrigger+"_"+util::toTString(runStart)+"-"+util::toTString(runEnd);
+  TString outNamePrefix = "TurnOn_"+jetType+"_"+probeTrigger+"_"+util::toTString(runStart)+"-"+util::toTString(runEnd);
 
   TCanvas *canEff = new TCanvas("canEff","Eff",500,500);
   canEff->cd();
   hFrame->Draw();
-  gEff->Draw("PE1same");
+  //gEff->Draw("PE1same");
+  hEff->Draw("PE1same");
   fit->Draw("same");
   lPtAve99->Draw("same");
   label->Draw("same");
@@ -221,7 +252,8 @@ void computeTriggerEfficiences(const TString &fileNames, int runStart, int runEn
   // Label events
   TLegend *legEvts = util::LabelFactory::createLegend(2);
   legEvts->AddEntry(hTotal,tagTrigger,"L");
-  legEvts->AddEntry(hPass,tagTrigger+" && "+probeTrigger,"L");
+  //legEvts->AddEntry(hPass,tagTrigger+" && "+probeTrigger,"L");
+  legEvts->AddEntry(hPass,probeTrigger,"L");
 
   TCanvas *canEvts = new TCanvas("canEvts","Evts",500,500);
   canEvts->cd();
@@ -235,20 +267,19 @@ void computeTriggerEfficiences(const TString &fileNames, int runStart, int runEn
 
 // --------------------------------------------------
 void run(int nEvts = -1) {
-  TString input = "~/lustre/data/Jet_Run2011A-PromptReco-v2_163337-163757/Jet_Run2011A-PromptReco-v2_163337-163757_ak5Calo.root";
+  TString input = "~/lustre/data/Jet_Run2011A-PromptReco-v2_163337-163757/Jet_Run2011A-PromptReco-v2_163337-163757_ak5PF.root";
   int runStart = 163337;
   int runEnd = 163757;
   double lumi = 62.;
   int nBins1 = 35;
   int nBins2 = 70;
 
-   computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve30","HltDiJetAve60",nBins1,40,80,nEvts);
-   computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve60","HltDiJetAve80",nBins1,65,130,nEvts);
-    computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve80","HltDiJetAve110",nBins1,80,150,nEvts);
-    computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve110","HltDiJetAve150",nBins1,120,200,nEvts);
-    computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve150","HltDiJetAve190",nBins2,160,270,nEvts);
-    computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve190","HltDiJetAve240",nBins2,200,320,nEvts);
-    computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve240","HltDiJetAve300",nBins2,250,380,nEvts);
-    computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve300","HltDiJetAve370",nBins2,350,450,nEvts);
-
+  computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve30","HltDiJetAve60",nBins1,40,100,nEvts);
+  computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve60","HltDiJetAve80",nBins1,65,130,nEvts);
+  computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve80","HltDiJetAve110",nBins1,80,150,nEvts);
+  computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve110","HltDiJetAve150",nBins1,120,200,nEvts);
+  computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve150","HltDiJetAve190",nBins2,160,270,nEvts);
+  computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve190","HltDiJetAve240",nBins2,200,320,nEvts);
+  computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve240","HltDiJetAve300",nBins2,250,380,nEvts);
+  computeTriggerEfficiences(input,runStart,runEnd,lumi,"HltDiJetAve300","HltDiJetAve370",nBins2,350,450,nEvts);
 }
