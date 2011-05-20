00b1427b020b2cbfefe669687055d35b84bbf9a1
mschrode <>
2011-05-20 09:50:46 +0000

    Add functionality to sort jets in pt

--- util/utils.h
+++ util/utils.h
@@ -1,8 +1,9 @@
-// $Id: utils.h,v 1.6 2010/12/05 22:19:26 mschrode Exp $
+// $Id: utils.h,v 1.7 2011/05/17 16:36:43 mschrode Exp $
 
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <algorithm>
 #include <cmath>
 #include <string>
 #include <sstream>
@@ -15,7 +16,7 @@
 //!
 //!  \author   Matthias Schroeder (www.desy.de/~matsch)
 //!  \date     2010/03/09
-//!  $Id: utils.h,v 1.6 2010/12/05 22:19:26 mschrode Exp $
+//!  $Id: utils.h,v 1.7 2011/05/17 16:36:43 mschrode Exp $
 // -------------------------------------------------------------------------------------
 namespace util {
 
@@ -104,53 +105,53 @@ namespace util {
   }
 
 
-/*   // For sorting jets in pt */
-/*   // -------------------------------------------------- */
-/*   class JetIndexCol { */
-/*   private: */
-/*     class Jet { */
-/*     public: */
-/*       Jet(unsigned int jetIdx, double jetPt) : idx_(jetIdx), pt_(jetPt) {}; */
-/* 	const unsigned int idx_; */
-/* 	const double pt_; */
-/* 	// For sorting jets in pt */
-/* 	static bool ptGreaterThan(const Jet *idx1, const Jet *idx2) { */
-/* 	  // check for 0 */
-/* 	  if(idx1 == 0) { */
-/* 	    return idx2 != 0; */
-/* 	  } else if (idx2 == 0) { */
-/* 	    return false; */
-/* 	  } else { */
-/* 	    return idx1->pt_ > idx2->pt_; */
-/* 	  } */
-/* 	} */
-/*     }; */
+  // For sorting jets in pt
+  // --------------------------------------------------
+  class JetIndexCol {
+  private:
+    class Jet {
+    public:
+      Jet(unsigned int jetIdx, double jetPt) : idx_(jetIdx), pt_(jetPt) {};
+	const unsigned int idx_;
+	const double pt_;
+	// For sorting jets in pt
+	static bool ptGreaterThan(const Jet *idx1, const Jet *idx2) {
+	  // check for 0
+	  if(idx1 == 0) {
+	    return idx2 != 0;
+	  } else if (idx2 == 0) {
+	    return false;
+	  } else {
+	    return idx1->pt_ > idx2->pt_;
+	  }
+	}
+    };
 
-/*     std::vector<Jet*> jets_; */
+    std::vector<Jet*> jets_;
 
 
-/*   public:  */
-/*     JetIndexCol() {} */
-/*     ~JetIndexCol() { clear(); } */
+  public:
+    JetIndexCol() {}
+    ~JetIndexCol() { clear(); }
     
-/*     unsigned int operator()(unsigned int i) { return idx(i); } */
-/*     unsigned int nJets() const { return jets_.size(); } */
-/*     unsigned int idx(unsigned int i) const { return jets_.at(i)->idx_; } */
-/*     double pt(unsigned int i) const { return jets_.at(i)->pt_; } */
+    unsigned int operator()(unsigned int i) { return idx(i); }
+    unsigned int nJets() const { return jets_.size(); }
+    unsigned int idx(unsigned int i) const { return jets_.at(i)->idx_; }
+    double pt(unsigned int i) const { return jets_.at(i)->pt_; }
     
-/*     void add(unsigned int jetIdx, double jetPt) {  */
-/*       jets_.push_back(new Jet(jetIdx,jetPt)); */
-/*     } */
-/*     void clear() { */
-/*       for(std::vector<Jet*>::iterator it = jets_.begin(); it != jets_.end(); ++it) { */
-/* 	delete *it; */
-/*       } */
-/*       jets_.clear(); */
-/*     } */
-/*     void sort() { */
-/*       std::sort(jets_.begin(),jets_.end(),Jet::ptGreaterThan); */
-/*     } */
-/*   }; */
+    void add(unsigned int jetIdx, double jetPt) {
+      jets_.push_back(new Jet(jetIdx,jetPt));
+    }
+    void clear() {
+      for(std::vector<Jet*>::iterator it = jets_.begin(); it != jets_.end(); ++it) {
+	delete *it;
+      }
+      jets_.clear();
+    }
+    void sort() {
+      std::sort(jets_.begin(),jets_.end(),Jet::ptGreaterThan);
+    }
+  };
 
 }
 #endif
