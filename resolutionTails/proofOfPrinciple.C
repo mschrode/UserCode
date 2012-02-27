8df93d865d2758d955efa6002e90f2d82cf18198
mschrode <>
2012-02-27 15:16:13 +0000

    tweak style

--- resolutionTails/proofOfPrinciple.C
+++ resolutionTails/proofOfPrinciple.C
@@ -1,4 +1,4 @@
-// $Id: $
+// $Id: proofOfPrinciple.C,v 1.1 2012/01/05 11:35:41 mschrode Exp $
 
 #include <cassert>
 #include <iostream>
@@ -11,6 +11,7 @@
 #include "TH1D.h"
 #include "TLatex.h"
 #include "TLine.h"
+#include "TPaveText.h"
 #include "TRandom.h"
 #include "TRandom3.h"
 #include "TString.h"
@@ -149,8 +150,8 @@ void proofOfPrinciple(unsigned int nEntries = 500000) {
 
   TLine* linTailResp = new TLine(gaussStart_,yMinResp,gaussStart_,4.);
   linTailResp->SetLineWidth(2);
-  linTailResp->SetLineColor(46);
-  linTailResp->SetLineStyle(2);
+  linTailResp->SetLineColor(28);
+  //  linTailResp->SetLineStyle(2);
   TArrow* arrResp = new TArrow(gaussStart_,1.5,tailStart_+0.09,1.5);
   arrResp->SetLineWidth(linTailResp->GetLineWidth());
   arrResp->SetLineColor(linTailResp->GetLineColor());
@@ -159,6 +160,14 @@ void proofOfPrinciple(unsigned int nEntries = 500000) {
   TLatex* arrRespLabel = new TLatex(tailStart_+0.19,1.85,"Tail");
   arrRespLabel->SetTextFont(42);
   arrRespLabel->SetTextColor(arrResp->GetLineColor());
+  TArrow* arrRespCore = new TArrow(gaussStart_,0.06,1.,0.06,0.05,"<-|");
+  arrRespCore->SetLineWidth(linTailResp->GetLineWidth());
+  arrRespCore->SetLineColor(linTailResp->GetLineColor());
+  arrRespCore->SetAngle(40);
+  TLatex* arrRespCoreLabel = new TLatex(gaussStart_+0.02,0.09,"2.5#sigma_{toy}");
+  arrRespCoreLabel->SetTextFont(42);
+  arrRespCoreLabel->SetTextColor(arrResp->GetLineColor());
+
 
   TLine* linTailAsymLeft = static_cast<TLine*>(linTailResp->Clone());
   linTailAsymLeft->SetX1(gaussStartAsym_);
@@ -179,11 +188,20 @@ void proofOfPrinciple(unsigned int nEntries = 500000) {
   TLatex* arrAsymLeftLabel = static_cast<TLatex*>(arrRespLabel->Clone());
   arrAsymLeftLabel->SetX(-0.56);
   arrAsymLeftLabel->SetY(1.85);
+  TArrow* arrAsymCore = static_cast<TArrow*>(arrRespCore->Clone());
+  arrAsymCore->SetX1(gaussStartAsym_);
+  arrAsymCore->SetX2(0.);
+  TLatex* arrAsymCoreLabel = new TLatex(gaussStartAsym_+0.01,0.12,"#frac{2.5}{#sqrt{2}}#sigma_{toy}");
+  arrAsymCoreLabel->SetTextFont(42);
+  arrAsymCoreLabel->SetTextColor(arrResp->GetLineColor());
 
-  TLegend* leg = util::LabelFactory::createLegendCol(3,0.35);
-  leg->AddEntry(hResp.front(),"f_{resp} #times "+util::toTString(varTailResp.front(),1),"L");
-  leg->AddEntry(hResp.at(nVar),"f_{resp} #times 1.0","L");
-  leg->AddEntry(hResp.back(),"f_{resp} #times "+util::toTString(varTailResp.back(),1),"L");
+  TPaveText* label = util::LabelFactory::createPaveText(1,-0.5);
+  label->AddText("Toy simulation");
+
+  TLegend* leg = util::LabelFactory::createLegendCol(5,0.3);
+  leg->AddEntry(hResp.front(),"f^{toy}_{resp} #times "+util::toTString(varTailResp.front(),1),"L");
+  leg->AddEntry(hResp.at(nVar),"f^{toy}_{resp} #times 1.0","L");
+  leg->AddEntry(hResp.back(),"f^{toy}_{resp} #times "+util::toTString(varTailResp.back(),1),"L");
 
   TCanvas* cResp = new TCanvas("cResp","Response",500,500);
   cResp->cd();
@@ -191,9 +209,12 @@ void proofOfPrinciple(unsigned int nEntries = 500000) {
   hResp.back()->Draw("Lsame");
   hResp.at(nVar)->Draw("Lsame");
   leg->Draw("same");
+  label->Draw("same");
   linTailResp->Draw("same");
   arrRespLabel->Draw("same");
   arrResp->Draw();
+  arrRespCore->Draw();
+  arrRespCoreLabel->Draw("same");
   cResp->SetLogy();
   cResp->SaveAs("TailStudy_ProofOfPrinciples_Response.eps","eps");
 
@@ -203,20 +224,24 @@ void proofOfPrinciple(unsigned int nEntries = 500000) {
   hAsym.back()->Draw("Lsame");
   hAsym.at(nVar)->Draw("Lsame");
   leg->Draw("same");
+  label->Draw("same");
   linTailAsymLeft->Draw("same");
   arrAsymLeftLabel->Draw("same");
   arrAsymLeft->Draw();
   linTailAsymRight->Draw("same");
   arrAsymRight->Draw();
+  arrAsymCore->Draw();
+  arrAsymCoreLabel->Draw("same");
   cAsym->SetLogy();
   cAsym->SaveAs("TailStudy_ProofOfPrinciples_Asymmetry.eps","eps");
 
   TGraph* gFAsymVsFResp = new TGraph(varTailResp.size(),&(varTailResp.front()),&(varTailAsym.front()));
   gFAsymVsFResp->SetMarkerStyle(20);
+  gFAsymVsFResp->SetMarkerSize(1.5);
 
   TCanvas* cFvsF = new TCanvas("cFvsF","FAsym vs FResp",500,500);
   cFvsF->cd();
-  TH1* hFrameFAsymVsFResp = new TH1D("hFrameFAsymVsFResp",";Scale of f_{resp};Scale of f_{asym}",1000,0.4,1.6);
+  TH1* hFrameFAsymVsFResp = new TH1D("hFrameFAsymVsFResp",";Scale of f^{toy}_{resp};Scale of f^{toy}_{asym}",1000,0.4,1.6);
   for(int bin = 1; bin <= hFrameFAsymVsFResp->GetNbinsX(); ++bin) {
     hFrameFAsymVsFResp->SetBinContent(bin,hFrameFAsymVsFResp->GetXaxis()->GetBinCenter(bin));
   }
@@ -224,14 +249,16 @@ void proofOfPrinciple(unsigned int nEntries = 500000) {
   hFrameFAsymVsFResp->GetYaxis()->SetRangeUser(0.4,1.6);
   hFrameFAsymVsFResp->Draw();
   gFAsymVsFResp->Draw("Psame");
+  label->Draw("same");
   cFvsF->SaveAs("TailStudy_ProofOfPrinciples_Variation.eps","eps");
 
   TGraph* gRelScale = new TGraph(varTailResp.size(),&(varTailResp.front()),&(relVarTailAsym.front()));
   gRelScale->SetMarkerStyle(20);
+  gRelScale->SetMarkerSize(1.5);
 
   TCanvas* cRelVar = new TCanvas("cRelVar","Relative variation",500,500);
   cRelVar->cd();
-  TH1* hFrameRelScale = new TH1D("hFrameRelScale",";Scale of f_{resp};Scale of f_{resp} / Scale of f_{asym}",1000,0.4,1.6);
+  TH1* hFrameRelScale = new TH1D("hFrameRelScale",";Scale of f^{toy}_{resp};Scale of f^{toy}_{asym} / Scale of f^{toy}_{resp}",1000,0.4,1.6);
   for(int bin = 1; bin <= hFrameRelScale->GetNbinsX(); ++bin) {
     hFrameRelScale->SetBinContent(bin,1.);
   }
@@ -239,6 +266,7 @@ void proofOfPrinciple(unsigned int nEntries = 500000) {
   hFrameRelScale->GetYaxis()->SetRangeUser(0.55,1.45);
   hFrameRelScale->Draw();
   gRelScale->Draw("Psame");
+  label->Draw("same");
   cRelVar->SaveAs("TailStudy_ProofOfPrinciples_RelVariation.eps","eps");
 }
 
