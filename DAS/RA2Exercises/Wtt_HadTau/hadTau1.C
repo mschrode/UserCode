66010a5c93bfd91bf9f2d25e61229cfe67977e26
mschrode <>
2013-01-09 22:04:29 +0000

    some tweaks

--- DAS/RA2Exercises/Wtt_HadTau/hadTau1.C
+++ DAS/RA2Exercises/Wtt_HadTau/hadTau1.C
@@ -8,13 +8,14 @@ const float kHtJetEtaMax  = 2.5;
 // === Main Script =====================================================
 void hadTau1(int nEvts = -1) {
   // --- Declare the Output Histograms ---------------------------------
-  TH1* hGenMuPt = new TH1F("hGenMuPt",";p_{T}(#mu^{gen});N(#mu^{gen}) [GeV]",30,0,300);
-  TH1* hGenTauPt = new TH1F("hGenTauPt",";p_{T}(#tau^{gen});N(#tau^{gen}) [GeV]",30,0,300);
+  TH1* hGenMuPt = new TH1F("hGenMuPt",";p_{T}(#mu^{gen}) [GeV];N(#mu^{gen})",30,0,300);
+  TH1* hGenTauPt = static_cast<TH1*>(hGenMuPt->Clone("hGenTauPt"));
+  hGenTauPt->SetTitle(";p_{T}(#tau^{gen}) [GeV];N(#tau^{gen})");
 
 
   // --- Declare the Variables Read from the Tree ----------------------
   // Array dimensions
-  const int kRecoJetColSize = 200;
+  const int kRecoJetColSize = 10;
   const int kGenLepColSize = 6;
 
   // Reco-level jets
@@ -40,7 +41,8 @@ void hadTau1(int nEvts = -1) {
 
   // Get the tree from file
   TChain* tr = new TChain("AnaTree");
-  tr->Add("/nfs/dust/test/cmsdas/school61/susy/ntuple/test/WJets.root");
+  //  tr->Add("/nfs/dust/test/cmsdas/school61/susy/ntuple/test/WJets.root");
+  tr->Add("/afs/desy.de/project/cmsdas/SUSY/ntuple/test/WJets.root");
 
   // Set the branches
   tr->SetBranchAddress("NrecoJet",&nRecoJets);
@@ -64,7 +66,6 @@ void hadTau1(int nEvts = -1) {
   // Auxiliary variables (initialize outside event loop
   // for performance reasons)
   TLorentzVector aux4Vector;
-  std::set<int> auxIdcsLeptonJets;
 
   // Loop over the tree entries
   for(int evtIdx = 0; evtIdx < nEvtsToProcess; ++evtIdx) {
@@ -111,23 +112,21 @@ void hadTau1(int nEvts = -1) {
 
 
 
-    // "Cross cleaning": find the jet(s) that
+    // "Cross cleaning": find the jet that
     // - is the muon (each muon is also considered a Particle-Flow jet!)
-    // - originate in the hadronic-tau decay
-    // Associate jets and leptons if they are closer in eta-phi space
+    // - originates in the hadronic-tau decay
+    // Associate jet and lepton if they are closer in eta-phi space
     // than deltaRMax
-    auxIdcsLeptonJets.clear();
+    int leptonJetIdx = -1;
     float deltaRMax = 0.1;
     if( flgW == 15 && genLepPt < 50. ) deltaRMax = 0.2;
-    findMatchedJets(auxIdcsLeptonJets,recoJetEta,recoJetPhi,nRecoJets,genLepEta,genLepPhi,deltaRMax);
-
+    if( !findMatchedJet(leptonJetIdx,recoJetEta,recoJetPhi,nRecoJets,genLepEta,genLepPhi,deltaRMax) ) continue; // Want unambigious matching (exactly one jet within deltaRMax)
 
     // Calculate RA2 selection-variables from "cleaned" jets
     float selNJet = 0; // Number of jets with pt > 50 GeV and |eta| < 2.5 (HT jets)
-
     for(int jetIdx = 0; jetIdx < nRecoJets; ++jetIdx) {	// Loop over reco jets
       // Skip this jet if it is the lepton
-      if( auxIdcsLeptonJets.find(jetIdx) != auxIdcsLeptonJets.end() ) continue;
+      if( jetIdx == leptonJetIdx ) continue;
 
       // Calculate NJet 
       if( recoJetPt[jetIdx] > kHtJetPtMin && TMath::Abs(recoJetEta[jetIdx]) < kHtJetEtaMax ) selNJet++;
@@ -143,7 +142,6 @@ void hadTau1(int nEvts = -1) {
 
   } // End of loop over tree entries
 
-
   // --- Save the Histograms to File -----------------------------------
   TFile outFile("HadTau_WJetMC.root","RECREATE");
   hGenMuPt->Write();
@@ -156,12 +154,27 @@ void hadTau1(int nEvts = -1) {
 
 // === Auxiliary Functions =======================================
 
-void findMatchedJets(std::set<int> &jetIdcs, const float* jetEta, const float* jetPhi, int nJets, float lepEta, float lepPhi, float deltaRMax) {
+// Find index 'matchedJetIdx' of the jet that is within deltaRMax
+// around the lepton. Returns
+//  - true  : if exactly one jet has been found.
+//  - false : otherwise. In that case, 'matchedJetIdx == -1'
+bool findMatchedJet(int &matchedJetIdx, const float* jetEta, const float* jetPhi, int nJets, float lepEta, float lepPhi, float deltaRMax) {
+  bool unambigiousMatch = false;
+  matchedJetIdx = -1;
   for(int jetIdx = 0; jetIdx < nJets; ++jetIdx) { // Loop over jets
     if( deltaR(jetEta[jetIdx],lepEta,jetPhi[jetIdx],lepPhi) < deltaRMax ) {
-      jetIdcs.insert(jetIdx);
+      // Check if a jet has already been matched
+      if( unambigiousMatch ) {
+	matchedJetIdx = -1;
+	unambigiousMatch = false; // We only want exactly one matched jet
+	break;
+      }
+      matchedJetIdx = jetIdx;
+      unambigiousMatch = true;
     }
   } // End of loop over jets
+
+  return unambigiousMatch;
 }
 
 
