f2f5f2c2e66f253682fbb0febc3b11956163711f
fcostanz <>
2013-01-16 08:23:31 +0000

    start

--- DAS/RA2Exercises/QCD_RS/bin/Rebalance.cc
+++ DAS/RA2Exercises/QCD_RS/bin/Rebalance.cc
@@ -17,131 +17,6 @@ struct Jet{
   float Pt,Px,Py,Pz,E,Phi,Eta;
 };
 
-
-bool Rebalance_KinFitter( const Event* evt, Event* rebalanced,  JetResolution * JetRes=0)
-{
-   //// Interface to KinFitter
-   TKinFitter* myFit = new TKinFitter();
-   std::vector<TLorentzVector*> lvec_m;
-   std::vector<TMatrixD*> covMat_m;
-   std::vector<TFitParticleEtEtaPhi*> fitted;
-   std::vector<TFitParticleEtEtaPhi*> measured;
-   
-   bool result = true;
-   double MHTx_high = 0;
-   double MHTy_high = 0;
-   std::vector<Jet> rebjets;
-
-   //// Fill measured particles to vector
-   for (int i = 0; i<evt->NrecoJet; ++i) {
-
-      if (evt->recoJetPt[i] >= 10. ) {  //use all jets >= 20. GeV
-         MHTx_high -= evt->recoJetPx[i];
-         MHTy_high -= evt->recoJetPy[i];
-
-         // The particles before fitting
-         double tmppx, tmppy, tmppz, tmpe;
-         tmppx = evt->recoJetPx[i];
-         tmppy = evt->recoJetPy[i];
-         tmppz = evt->recoJetPz[i];
-         tmpe  = evt->recoJetE[i];
-
-         TLorentzVector* lv = new TLorentzVector(tmppx, tmppy, tmppz, tmpe);
-         lvec_m.push_back(lv);
-         TMatrixD* cM = new TMatrixD(3, 3);
-         (*cM)(0, 0) = JetRes->JetResolution_Pt2(evt->recoJetPt[i], evt->recoJetEta[i], i);
-         (*cM)(1, 1) = JetRes->JetResolution_Eta2(evt->recoJetE[i], evt->recoJetEta[i]);
-         (*cM)(2, 2) = JetRes->JetResolution_Phi2(evt->recoJetE[i], evt->recoJetEta[i]);
-         covMat_m.push_back(cM);
-         char name[10];
-         sprintf(name, "jet%i", i);
-         TFitParticleEtEtaPhi* jet1 = new TFitParticleEtEtaPhi(name, name, lvec_m.back(), covMat_m.back());
-         measured.push_back(jet1);
-         TFitParticleEtEtaPhi* jet2 = new TFitParticleEtEtaPhi(name, name, lvec_m.back(), covMat_m.back());
-         fitted.push_back(jet2);
-         myFit->addMeasParticle(fitted.back());
-      }
-      else {
-	  rebjets.push_back(Jet( 
-	       evt->recoJetPt[i],
-	       evt->recoJetPx[i],evt->recoJetPy[i],evt->recoJetPz[i],
-	       evt->recoJetE[i],evt->recoJetPhi[i],evt->recoJetEta[i]));
-      }
-   }
-
-   //// Add momentum constraints
-   double MET_constraint_x = 0.;
-   double MET_constraint_y = 0.;
-   TFitConstraintEp* momentumConstr1 = new TFitConstraintEp("px", "px", 0, TFitConstraintEp::pX, MET_constraint_x);
-   TFitConstraintEp* momentumConstr2 = new TFitConstraintEp("py", "py", 0, TFitConstraintEp::pY, MET_constraint_y);
-   for (unsigned int i = 0; i < fitted.size(); ++i) {
-      momentumConstr1->addParticle(fitted.at(i));
-      momentumConstr2->addParticle(fitted.at(i));
-   }
-   myFit->addConstraint(momentumConstr1);
-   myFit->addConstraint(momentumConstr2);
-
-   //// Set fit parameters
-   myFit->setVerbosity(0);
-   myFit->setMaxNbIter(100);
-   myFit->setMaxF(0.01 * 2);
-   myFit->setMaxDeltaS(1.e-3);
-   myFit->fit();
-   int status = myFit->getStatus();
-
-   double chi2 = 0;
-   double F = 0;
-   double prob = 0;
-   if (status == 0) {
-      chi2 = myFit->getS();
-      F = myFit->getF();
-      int dof = myFit->getNDF();
-      prob = TMath::Prob(chi2, dof);
-      if (prob < 1.e-2) result = false;
-   } else {
-      chi2 = 99999;
-      prob = 0;
-      F = 99999;
-      result = false;
-   }
-   //   h_fitProb->Fill(prob);  /// plot fit probability
-
-   //// Get the output of KinFitter
-   rebalanced->CopyEvent( *evt );
-   //rebalanced->NrecoJet = measured.size();
-   for (unsigned int i = 0; i < measured.size(); ++i) {
-          rebjets.push_back(Jet( 
-	         fitted.at(i)->getCurr4Vec()->Pt(),
-	         fitted.at(i)->getCurr4Vec()->Px(),fitted.at(i)->getCurr4Vec()->Py(),
-		 fitted.at(i)->getCurr4Vec()->Pz(),fitted.at(i)->getCurr4Vec()->E(),
-		 fitted.at(i)->getCurr4Vec()->Phi(),fitted.at(i)->getCurr4Vec()->Eta()));
-   }
-   
-   std::sort(rebjets.begin(), rebjets.end());
-   for (std::vector<Jet>::const_iterator jet=rebjets.begin(); jet!=rebjets.end(); ++jet){
-      rebalanced->recoJetPt[jet-rebjets.begin()] = jet->Pt;
-      rebalanced->recoJetPx[jet-rebjets.begin()] = jet->Px;
-      rebalanced->recoJetPy[jet-rebjets.begin()] = jet->Py;
-      rebalanced->recoJetPz[jet-rebjets.begin()] = jet->Pz;
-      rebalanced->recoJetE[ jet-rebjets.begin()] = jet->E;
-      rebalanced->recoJetPhi[jet-rebjets.begin()] = jet->Phi;
-      rebalanced->recoJetEta[jet-rebjets.begin()] = jet->Eta;
-   }  
-
-
-   delete myFit;
-   for (unsigned int i = 0; i < measured.size(); ++i) {
-      delete lvec_m.at(i);
-      delete covMat_m.at(i);
-      delete measured.at(i);
-      delete fitted.at(i);
-   }
-   delete momentumConstr1;
-   delete momentumConstr2;
-
-   return result;
-}
-
 // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 //
 //  your code goes here:
@@ -195,8 +70,7 @@ void Rebalance( const std::vector<Event*>& evts, std::vector<Event*>& rebalanced
   std::cerr<<"...rebalancing: ";
   for (std::vector<Event*>::const_iterator it=evts.begin(); it!=evts.end(); ++it){
     Event * rebalanced = new Event;
-//    if ( Rebalance( *it, rebalanced, JetRes ) ) {
-    if ( Rebalance_KinFitter( *it, rebalanced, JetRes ) ) {
+    if ( Rebalance( *it, rebalanced, JetRes ) ) {
       rebalanced_events.push_back( rebalanced );
     } else {
       ++not_converged;
--- DAS/RA2Exercises/QCD_RS/bin/Smear.cc
+++ DAS/RA2Exercises/QCD_RS/bin/Smear.cc
@@ -14,43 +14,6 @@ struct Jet{
   float Pt,Px,Py,Pz,E,Phi,Eta;
 };
 
-void Smear_Solution( const Event*evt, Event* rs, JetResolution * JetRes )
-{
-  float w = evt->EvtWgt;
-  
-  std::vector<Jet> smeared;
-  int ith_jet = 0;
-  for (int i=0; i<evt->NrecoJet; ++i) {
-    if (evt->recoJetPt[i] > 30. ) { //Smear only jets above 30.
-      float rand = JetRes->GetRandom( evt->recoJetPt[i], evt->recoJetEta[i], ith_jet++ );
-      smeared.push_back(Jet( 
-			    evt->recoJetPt[i]*rand,
-			    evt->recoJetPx[i]*rand,evt->recoJetPy[i]*rand,evt->recoJetPz[i]*rand,
-			    evt->recoJetE[i]*rand,evt->recoJetPhi[i],evt->recoJetEta[i]));
-	} else {
-      smeared.push_back(Jet( 
-			    evt->recoJetPt[i],
-			    evt->recoJetPx[i],evt->recoJetPy[i],evt->recoJetPz[i],
-			    evt->recoJetE[i],evt->recoJetPhi[i],evt->recoJetEta[i]));
-    }
-  }
-  //sort jet in pT:
-  std::sort(smeared.begin(), smeared.end());
-  
-  rs->CopyEvent( *evt );
-  rs->EvtWgt = w;
-  for (std::vector<Jet>::const_iterator jet=smeared.begin(); jet!=smeared.end(); ++jet){
-    if ( jet-smeared.begin() >= rs->NrecoJet ) break;
-    rs->recoJetPt[jet-smeared.begin()] = jet->Pt;
-    rs->recoJetPx[jet-smeared.begin()] = jet->Px;
-    rs->recoJetPy[jet-smeared.begin()] = jet->Py;
-    rs->recoJetPz[jet-smeared.begin()] = jet->Pz;
-    rs->recoJetE[ jet-smeared.begin()] = jet->E;
-    rs->recoJetPhi[jet-smeared.begin()] = jet->Phi;
-    rs->recoJetEta[jet-smeared.begin()] = jet->Eta;
-  }	
-  smeared.clear();
-}
 
 // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 //
@@ -99,7 +62,7 @@ void Smear( const std::vector<Event*>& evts, std::vector<Event*>& rs_events )
       //sample the event evt 'Ntries' times:
       for (int j = 1; j <= Ntries; ++j) {
 	Event * rs_event = new Event;
-	Smear_Solution( *it, rs_event, JetRes );
+	Smear( *it, rs_event, JetRes );
 	rs_event->EvtWgt /= (float)Sample_N_times;
 	rs_event->EvtWgt /= (float)Ntries;
 	rs_events.push_back( rs_event );
